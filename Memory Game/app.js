9da0c18757cc7078f9d2dec36fb2cc611c5e5625
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-08-29 23:24:51 +0600

    Memory game

---
+++ Memory Game/app.js
@@ -0,0 +1,28 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png', 
+    }
+];
+
+console.log(cardArray)
